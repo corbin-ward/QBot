0848f26661d4dd1738d21cc683f379c1ae4869c5
Corbin Ward <corbinwinbox@gmail.com>
2024-07-11 18:11:55 -0700

    Added Database Connection

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,24 @@
 const { Events } = require('discord.js');
+const mongoose = require('mongoose');
+const { mongoURI } = require('../config.json');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+		client.user.setActivity({
+			name: "Ready to queue up?"
+		});
+
+		await mongoose.connect(mongoURI, {
+		});
+
+		if(mongoose.connect) {
+			console.log('Connected to database.');
+		} else {
+			console.log('Failed to connect to database.');
+		}
+
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
